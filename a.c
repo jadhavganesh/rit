c16ec22bf65c17ec26d951409e715e410f0493c7
Ganesh <jadhavganesh0301@gmail.com>
2017-04-29 12:54:24 +0530

    hello moto
    new chage is created....

--- a.c
+++ a.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int  main()
 {
-int a,b,c;
+int a,b,c,d;
 printf("enter the a & b");
 scanf("%d%d",&a,&b);
 c=a+b;
-printf("%d",c);
+d=a-b;
+printf("sum is %d",c);
+printf("Sub is %d",d);
 return 0;
 }
